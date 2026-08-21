234be39146c2034dde9b754b0e327c2c173ae16a
Sarthak Mishra <sarthakjm2284@gmail.com>
2026-08-21 16:51:50 +0530

    chore: configure Tailwind css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,15 @@
 function App() {
   return (
-    <main>
-      <h1>DOOMSDAY</h1>
-      <p>The world is running out of time.</p>
+    <main className="min-h-screen bg-black flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold text-red-600">
+          DOOMSDAY
+        </h1>
+
+        <p className="mt-4 text-xl text-gray-400">
+          The world is running out of time.
+        </p>
+      </div>
     </main>
   )
 }
